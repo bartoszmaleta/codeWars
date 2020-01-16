2ac0e2d4b71e294a24cd60a0b4a09b525a8c3f41
Bartosz Maleta <bartosz.maleta@gmail.com>
2020-01-16 16:20:59 +0100

    Add Ghost and Cube exercise

---
+++ src/kyu8/Ghost.java
@@ -0,0 +1,35 @@
+package kyu8;
+
+import java.util.Random;
+import java.util.ArrayList;
+
+public class Ghost {
+    private Random rand = new Random();
+
+    public Ghost() {
+    }
+
+    public static void main(String[] args) {
+    }
+
+    public String getColor() {
+        String yellowColour = "yellow";
+        String whiteColour = "white";
+        String redColour = "red";
+        String purpleColour = "purple";
+        ArrayList<String> colours = new ArrayList<>();
+        colours.add(whiteColour);
+        colours.add(redColour);
+        colours.add(yellowColour);
+        colours.add(purpleColour);
+
+        // for (int i = 0; i < 1; i++) {
+        // System.out.println(colours.get(rand.nextInt(colours.size())));
+        // String randomColour = colours.get(rand.nextInt(colours.size()));
+        // return randomColour;
+
+        String randomColour = colours.get(rand.nextInt(colours.size()));
+        System.out.println(randomColour);
+        return randomColour;
+    }
+}
