797233a431f202ec28bddfb885c422128ec116f8
Bartosz Maleta <bartosz.maleta@gmail.com>
2020-01-18 17:41:47 +0100

    Add Grasshopper - Messi exe

---
+++ src/kyu8/Goals.java
@@ -0,0 +1,9 @@
+package kyu8;
+
+public class Goals {
+    public static int goals(int laLigaGoals, int copaDelReyGoals, int championsLeagueGoals) {
+        int totalGoals = laLigaGoals + copaDelReyGoals + championsLeagueGoals;
+
+        return totalGoals;
+    }
+}
