58b2a4d718a79724540a7453e1e4bbdf45ec2e49
Bartosz Maleta <bartosz.maleta@gmail.com>
2020-01-18 18:27:26 +0100

    Add BMI Calculator

---
+++ src/kyu8/CalculatorBMI.java
@@ -0,0 +1,18 @@
+package kyu8;
+
+public class CalculatorBMI {
+    public static String bmi(double weight, double height) {
+        double bmi = weight / (height * height);
+        String outputMessage = "";
+        if (bmi > 30) {
+            outputMessage = "Obese";
+        } else if (bmi > 25) {
+            outputMessage = "Overweight";
+        } else if (bmi > 18.5) {
+            outputMessage = "Normal";
+        } else if (bmi <= 18.5) {
+            outputMessage = "Underweight";
+        }
+        return outputMessage;
+      }
+}
