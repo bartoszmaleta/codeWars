15cd657e6b4230051d25dd1b0dff5c760fccd62b
Bartosz Maleta <bartosz.maleta@gmail.com>
2020-01-19 17:00:49 +0100

    Add DistanceFuelCalc

---
+++ src/kyu8/FuelCalc.java
@@ -0,0 +1,10 @@
+package kyu8;
+
+public class FuelCalc {
+    public static boolean zeroFuel(double distanceToPump, double mpg, double fuelLeft) {
+        if (mpg * fuelLeft < distanceToPump) {
+            return false;
+        }
+        return true;
+    }
+}
