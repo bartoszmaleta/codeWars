f09d958fc8c1300eff7848e3c19118028485bdcd
Bartosz Maleta <bartosz.maleta@gmail.com>
2020-01-21 13:08:18 +0100

    Add ArrayInRange

---
+++ src/kyu8/AddingInterNumbersToArray.java
@@ -0,0 +1,46 @@
+package kyu8;
+
+import java.util.Arrays;
+
+public class AddingInterNumbersToArray {
+    public static int[] pipeFix(int[] numbers) {
+        int minimumValueOfArray = Integer.MAX_VALUE;
+        int maximumValueOfArray = Integer.MIN_VALUE;
+
+        for (int i = 0; i < numbers.length; i++) {
+            if (numbers[i] > maximumValueOfArray) {
+                maximumValueOfArray = numbers[i];
+            }
+        }
+
+        minimumValueOfArray = numbers[0];
+        // for (int i = 0; i < numbers.length; i++) {
+        // if (numbers[i] < minimumValueOfArray) {
+        // minimumValueOfArray = numbers[i];
+        // }
+        // }
+
+        System.out.println("min = " + minimumValueOfArray);
+        System.out.println("max = " + maximumValueOfArray);
+
+        int[] filledArray = new int[maximumValueOfArray - minimumValueOfArray + 1];
+        System.out.println("len = " + filledArray.length);
+
+        int counter = 0;
+        while (minimumValueOfArray <= maximumValueOfArray) {
+            filledArray[counter] = minimumValueOfArray;
+            counter++;
+            minimumValueOfArray++;
+        }
+
+        System.out.println(Arrays.toString(filledArray));
+        return filledArray;
+    }
+
+    public static void main(String[] args) {
+        // int[] arraMy = {3, 5, 4, 7, 8, 9, 11};
+        int[] arraMy = { 14, 0, 0, 0, 0, 527 };
+        int[] newArray = pipeFix(arraMy);
+        System.out.println(newArray.length);
+    }
+}
