4b4c2a07b0ae5a73524dd895e960b77791e4cd5c
Bartosz Maleta <bartosz.maleta@gmail.com>
2020-03-21 20:58:37 +0100

    Add sortMyTextBooks

---
+++ src/kyu8/sortMyTextBooks.java
@@ -0,0 +1,12 @@
+package kyu8;
+
+import java.util.Collections;
+import java.util.List;
+
+public class sortMyTextBooks {
+
+    public static List<String> sort(List<String> textbooks) {
+        Collections.sort(textbooks);
+        return textbooks;
+    }
+}
