a5d68f6746da5e5a5e64d5e5c5cfbc4910aef81d
Bartosz Maleta <bartosz.maleta@gmail.com>
2020-03-22 20:35:29 +0100

    Add AreaOfCircle

---
+++ src/kyu8/AreaOfCircle.java
@@ -0,0 +1,16 @@
+package kyu8;
+
+import static java.lang.Math.PI;
+import static java.lang.Math.pow;
+
+public class AreaOfCircle {
+
+    public static double area(double radius) {
+        if (radius <= 0) {
+            throw new IllegalArgumentException();
+        }
+
+        double area = PI * pow(radius, 2);
+        return area;
+    }
+}
