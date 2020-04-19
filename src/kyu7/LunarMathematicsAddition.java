9ec16736dcd046650eff9f4579c413962159ecf3
Bartosz Maleta <bartosz.maleta@gmail.com>
2020-04-19 15:32:37 +0200

    Add LunarMathematicsAddition

--- src/kyu7/LunarMathematicsAddition.java
+++ src/kyu7/LunarMathematicsAddition.java
@@ -1,101 +1,22 @@
 package kyu7;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Iterator;
-import java.util.List;
-
 public class LunarMathematicsAddition {
-    public static long add(long number1, long number2)
-    {
-        List<Integer> resultListInteger = new ArrayList<>();
-
-        String number1String = String.valueOf(number1);
-        String number2String = String.valueOf(number2);
-
-        String[] number1Array = number1String.split("");
-        List<String> number1ListString = new ArrayList<>();
-        number1ListString = Arrays.asList(number1Array);
-
-        String[] number2Array = number2String.split("");
-        List<String> number2ListString = new ArrayList<>();
-        number2ListString = Arrays.asList(number2Array);
-
-        List<Integer> number1ListInteger = new ArrayList<>();
-        List<Integer> number2ListInteger = new ArrayList<>();
-
-        for (String str : number1ListString) {
-            Integer number = Integer.parseInt(str);
-            number1ListInteger.add(number);
+    public static long add(long number1, long number2) {
+        long sum = 0;
+        for (int i = 1; number1 > 0 || number2 > 0; number1 /= 10, number2 /= 10, i *= 10) {
+            System.out.println("number1 = " + number1);
+            System.out.println("number2 = " + number2);
+            long numberToPrint = i * Math.max(number1 % 10, number2 % 10);
+            System.out.println("number to add = " + numberToPrint + "\n");
+            sum += numberToPrint;
         }
-
-
-        for (String str : number2ListString) {
-            Integer number = Integer.parseInt(str);
-            number2ListInteger.add(number);
-        }
-
-        System.out.println("number 1 list string " + number1ListString);
-        System.out.println("number 2 list string " + number2ListString);
-
-
-        System.out.println("number 1 list integer " + number1ListInteger);
-        System.out.println("number 2 list integer " + number2ListInteger);
-
-        Iterator<Integer> iteratorNumber1 = number1ListInteger.iterator();
-        Iterator<Integer> iteratorNumber2 = number2ListInteger.iterator();
-
-//        int counter = 0;
-//        while (counter < number2ListInteger.size()) {
-//            int numberOfNumber1List = number1ListInteger.get(counter);
-//            int numberOfNumber2List = number2ListInteger.get(counter);
-//
-//            if (numberOfNumber2List < numberOfNumber1List) {
-//                resultListInteger.add(numberOfNumber1List);
-//            } else {
-//                resultListInteger.add(numberOfNumber2List);
-//            }
-//        }
-
-        for (int i = 0; i < number2ListInteger.size(); i++) {
-            int numberOfNumber1List = number1ListInteger.get(i);
-            int numberOfNumber2List = number2ListInteger.get(i);
-
-            System.out.println(numberOfNumber1List);
-            System.out.println(numberOfNumber2List);
-
-            if (numberOfNumber2List < numberOfNumber1List) {
-                resultListInteger.add(numberOfNumber1List);
-            } else {
-                resultListInteger.add(numberOfNumber2List);
-            }
-        }
-
-
-
-//        while(iteratorNumber2.hasNext()) {
-//            System.out.println("iterator number2 = " + iteratorNumber2.next());
-//            System.out.println("iterator number1 = " + iteratorNumber1.next());
-//
-//            if (iteratorNumber2.next() < iteratorNumber1.next()) {
-//                resultListInteger.add(iteratorNumber1.next());
-//            } else {
-//                resultListInteger.add(iteratorNumber2.next());
-//            }
-//        }
-
-        System.out.println("result list integer = " + resultListInteger);
-
-
-
-
-
-        /* The moon people believe in you. */
-        return -1;
+        return sum;
     }
 
     public static void main(String[] args) {
-        add(322, 410);
+        System.out.println(add(322, 4102));
+        System.out.println("---------------------");
+        System.out.println(add(4102, 322));
     }
 }
 
