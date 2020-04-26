390576eba32ececd3b8dafbaa5410cc9d19da83a
Bartosz Maleta <bartosz.maleta@gmail.com>
2020-04-26 16:37:31 +0200

    Add another solutions

--- src/javascript/kyu8/SimpleCalculator.js
+++ src/javascript/kyu8/SimpleCalculator.js
@@ -12,3 +12,23 @@ function calculator(a,b,sign){
       };
 
 };
+
+// ANTOHER SOLUTIONs
+function calculator(a,b,sign){
+  switch(sign)
+  {
+  case '+': return a + b;
+  case '-': return a - b;
+  case '*': return a * b;
+  case '/': return a / b;
+  default: return "unknown value";
+  }
+
+}
+
+function calculator(a,b,sign) {
+  try
+    { return eval(a+sign+b); }
+  catch(e)
+    { return "unknown value"; }
+}
