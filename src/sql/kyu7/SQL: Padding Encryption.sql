1cdf588334dfa3dedc31e8af6f885cc2dd2766ed
Bartosz Maleta <bartosz.maleta@gmail.com>
2020-06-04 15:39:07 +0200

    Add repeat() and length() example

---
+++ src/sql/kyu7/SQL: Padding Encryption.sql
@@ -0,0 +1,12 @@
+/*  SQL  */
+SELECT
+
+CONCAT(md5, REPEAT('1', LENGTH(sha256) - LENGTH(md5)))
+AS md5,
+
+CONCAT(REPEAT('0', LENGTH(sha256) - LENGTH(sha1)), sha1)
+AS sha1,
+
+sha256
+
+FROM encryption
