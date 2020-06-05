126c046f80c3d3abdefeb20a5810ada7d4f9bc57
Bartosz Maleta <bartosz.maleta@gmail.com>
2020-06-05 13:28:28 +0200

    Add algorithm with ceil

---
+++ src/sql/kyu7/Simple Fun #74: Growing Plant.sql
@@ -0,0 +1,9 @@
+select * from growing_plant;
+
+SELECT
+id,
+CASE
+  WHEN desired_height <= up_speed THEN 1
+  ELSE CEIL((desired_height - up_speed)::decimal / (up_speed - down_speed))::int + 1
+END AS num_days
+FROM growing_plant
