4b6119a4f8c2de00f3315a88736669f570310564
Bartosz Maleta <bartosz.maleta@gmail.com>
2020-06-05 13:12:03 +0200

    Add algorithm solution

---
+++ src/python/GrowingPlant.py
@@ -0,0 +1,10 @@
+def growing_plant(up_speed, down_speed, desired_height):
+    #your code here
+    num_day, actual_height = 0, 0
+    while True:
+      num_day += 1
+      actual_height += up_speed
+      if actual_height >= desired_height:
+        return num_day
+      else:
+        actual_height -= down_speed
