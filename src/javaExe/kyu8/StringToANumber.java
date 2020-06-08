6d916d4f989164d3aeea04d27bddb1bc826061bd
Bartosz Maleta <bartosz.maleta@gmail.com>
2020-06-08 10:18:48 +0200

    Add string to int

---
+++ src/javaExe/kyu8/StringToANumber.java
@@ -0,0 +1,8 @@
+package javaExe.kyu8;
+public class StringToANumber {
+    public static int stringToNumber(String str) {
+        //TODO: Convert str into a number
+        int num = Integer.valueOf(str);
+        return num;
+    }
+}
