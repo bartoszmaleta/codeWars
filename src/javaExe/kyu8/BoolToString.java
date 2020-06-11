8f83c143644e0350f662f556356d2bc683e7c06a
Bartosz Maleta <bartosz.maleta@gmail.com>
2020-06-11 13:18:31 +0200

    Add boolToString

---
+++ src/javaExe/kyu8/BoolToString.java
@@ -0,0 +1,11 @@
+package javaExe.kyu8;
+
+public class BoolToString {
+    public static String convert(boolean b){
+        if(b) {
+            return "true";
+        } else {
+            return "false";
+        }
+    }
+}
