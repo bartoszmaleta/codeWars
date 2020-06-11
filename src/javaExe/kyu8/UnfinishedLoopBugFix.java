396ac7ff1d6956c93934f829d9fecff16f3c1690
Bartosz Maleta <bartosz.maleta@gmail.com>
2020-06-12 00:32:24 +0200

    Add bugFix - infinite loop exercise

---
+++ src/javaExe/kyu8/UnfinishedLoopBugFix.java
@@ -0,0 +1,18 @@
+package javaExe.kyu8;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class UnfinishedLoopBugFix {
+    public static List CreateList(int number)
+    {
+        List list = new ArrayList();
+
+        for(int count = 1; count <= number; count++)
+        {
+            list.add(count);
+        }
+
+        return list;
+    }
+}
