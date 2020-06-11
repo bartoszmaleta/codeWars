c33904f2328e91b6b363917cb27ab2263d8de71c
Bartosz Maleta <bartosz.maleta@gmail.com>
2020-06-11 13:23:12 +0200

    Add isDigit()

---
+++ src/javaExe/kyu8/IsItANumber.java
@@ -0,0 +1,15 @@
+package javaExe.kyu8;
+
+public class IsItANumber {
+    public boolean isDigit(String s) {
+        if (s == null) {
+            return false;
+        }
+        try {
+            double d = Double.parseDouble(s);
+        } catch (NumberFormatException nfe) {
+            return false;
+        }
+        return true;
+    }
+}
