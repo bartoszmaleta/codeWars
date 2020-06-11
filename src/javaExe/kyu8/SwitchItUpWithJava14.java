fc263a2ef66728687e111b8a80ae7b96b56186a3
Bartosz Maleta <bartosz.maleta@gmail.com>
2020-06-11 19:45:57 +0200

    Add two switch exercise

---
+++ src/javaExe/kyu8/SwitchItUpWithJava14.java
@@ -0,0 +1,57 @@
+package javaExe.kyu8;
+
+public class SwitchItUpWithJava14 {
+    public static String switchItUp2(int number) {
+        String result = switch (number) {
+            case 0 -> "Zero";
+            case 1 -> "One";
+            case 2 -> "Two";
+            case 3 -> "Three";
+            case 4 -> "Fourth";
+            case 5 -> "Five";
+            case 6 -> "Six";
+            case 7 -> "Seven";
+            case 8 -> "Eight";
+            case 9 -> "Nine";
+            default -> throw new IllegalStateException("Unexpected value: " + number);
+        };
+        return null;
+    }
+
+    public static String switchItUp(int number) throws IllegalStateException {
+        String result = "";
+        switch (number) {
+            case 0:
+                result = "Zero";
+                break;
+            case 1:
+                result = "One";
+                break;
+            case 2:
+                result = "Two";
+                break;
+            case 3:
+                result = "Three";
+                break;
+            case 4:
+                result = "Fourth";
+                break;
+            case 5:
+                result = "Five";
+                break;
+            case 6:
+                result = "Six";
+                break;
+            case 7:
+                result = "Seven";
+                break;
+            case 8:
+                result = "Eight";
+                break;
+            case 9:
+                result = "Nine";
+                break;
+        }
+        return result;
+    }
+}
