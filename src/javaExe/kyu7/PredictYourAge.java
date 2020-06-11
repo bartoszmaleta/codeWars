4e425a7e131d221e382e00c57abdef9968dec357
Bartosz Maleta <bartosz.maleta@gmail.com>
2020-06-12 01:09:12 +0200

    Add another solution

--- src/javaExe/kyu7/PredictYourAge.java
+++ src/javaExe/kyu7/PredictYourAge.java
@@ -28,6 +28,13 @@ public class PredictYourAge {
 
     }
 
+    public static int predictAge2(int age1, int age2, int age3, int age4, int age5, int age6, int age7, int age8) {
+        int[] ages = { age1, age2, age3, age4, age5, age6, age7, age8 };
+        int sum = Arrays.stream(ages).map(age -> age * age).sum();
+        double sqrt = Math.sqrt(sum);
+        return (int) (sqrt / 2);
+    }
+
     public static void main(String[] args) {
         predictAge(65, 60, 75, 55, 60, 63, 64, 45);
     }
