fc9c029d9470e4e879b0afc941415e2f578f7674
Bartosz Maleta <bartosz.maleta@gmail.com>
2020-06-15 12:16:53 +0200

    Add another solution

--- src/sql/kyu6/Calculating Batting Average.sql
+++ src/sql/kyu6/Calculating Batting Average.sql
@@ -1,6 +1,8 @@
 SELECT
  player_name,
  games,
+-- ANTOHER OPTION:
+--   round(hits::numeric / at_bats, 3)::text as batting_average
  CAST(ROUND((hits + 0.0) / at_bats, 3) AS VARCHAR(10)) AS batting_average
 FROM yankees
 WHERE at_bats >= 100
