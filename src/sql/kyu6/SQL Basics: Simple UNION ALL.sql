5d398111eed28a63ee93ffb6847c4bea5f69f648
Bartosz Maleta <bartosz.maleta@gmail.com>
2020-06-16 15:11:06 +0200

    Add UNION ALL example

---
+++ src/sql/kyu6/SQL Basics: Simple UNION ALL.sql
@@ -0,0 +1,22 @@
+SELECT
+  'EU' AS location
+  , id
+  , name
+  , price
+  , card_name
+  , card_number
+  , transaction_date
+FROM eusales
+WHERE price > 50.00
+UNION ALL
+SELECT
+    'US' AS location
+  , id
+  , name
+  , price
+  , card_name
+  , card_number
+  , transaction_date
+FROM ussales
+WHERE price > 50.00
+ORDER BY location DESC, id
