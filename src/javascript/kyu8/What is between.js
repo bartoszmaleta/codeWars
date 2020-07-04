06b4ffea9f40b4f6335e55ad6f98b405a461032c
Bartosz Maleta <bartosz.maleta@gmail.com>
2020-07-04 20:22:47 +0200

    Add algo with for loop

---
+++ src/javascript/kyu8/What is between.js
@@ -0,0 +1,12 @@
+
+function between(a, b) {
+  // your code here
+  let arr = [];
+  for (let i = a; i <= b; i++) {
+    arr.push(i);
+    console.log(i);
+    console.log(arr);
+  }
+  console.log(arr);
+  return arr;
+}
