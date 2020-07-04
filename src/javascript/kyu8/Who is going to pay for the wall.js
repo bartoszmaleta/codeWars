5715a7b86884a613984896cebb93693166f955c7
Bartosz Maleta <bartosz.maleta@gmail.com>
2020-07-04 11:15:55 +0200

    Add substring and slice and array example

---
+++ src/javascript/kyu8/Who is going to pay for the wall.js
@@ -0,0 +1,24 @@
+function whoIsPaying(name){
+  //your code here
+  if (name.length < 2 || name === 'Me') {
+    let nameArray = [name];
+    return nameArray;
+  }
+
+  let firstTwoLetters = name.substring(0, 2);
+  let result = [name, firstTwoLetters];
+  return result;
+
+}
+
+
+function whoIsPaying(name) {
+    if (name.length <= 2) {
+        return [name];
+    }
+    return [name, name.slice(0, 2)];
+}
+
+function whoIsPaying(name) {
+    return (name.length > 2) ? ([name, name.substr(0, 2)]) : [name];
+}
