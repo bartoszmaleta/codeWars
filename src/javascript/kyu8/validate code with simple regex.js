0b437bd9c008d1379a2913d818502f94cf81ceb7
Bartosz Maleta <bartosz.maleta@gmail.com>
2020-07-14 13:54:04 +0200

    Refactor + string.repeat() example

--- src/javascript/kyu8/validate code with simple regex.js
+++ src/javascript/kyu8/validate code with simple regex.js
@@ -1,7 +1,8 @@
 function validateCode (code) {
   let fn = code.toString().charAt(0);
-  return fn === '1' || fn === '2' || fn === '3' ? true : false;
+  // return fn === '1' || fn === '2' || fn === '3' ? true : false;
+  return fn === '1' || fn === '2' || fn === '3';
 }
 
 // SECOND SOLUTION
-const validateCode = code => code.toString().charAt(0) === '1' || code.toString().charAt(0) === '2' || code.toString().charAt(0) === '3' ? true : false;
+const validateCode2 = code => code.toString().charAt(0) === '1' || code.toString().charAt(0) === '2' || code.toString().charAt(0) === '3';
