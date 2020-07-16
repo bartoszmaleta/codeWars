385c41dd1167e3de533aeb4327d6f3b00743645f
Bartosz Maleta <bartosz.maleta@gmail.com>
2020-07-16 14:05:00 +0200

    Add test() + includes() regex

--- src/javascript/kyu8/Do you speak "English"?.js
+++ src/javascript/kyu8/Do you speak "English"?.js
@@ -1,1 +1,12 @@
 const spEng = sentence => /english/gi.test(sentence);
+
+// SECOND SOLUTION
+function spEng2(sentence){
+    let sentenceLower = sentence.toLowerCase();
+    return sentenceLower.includes('english');
+}
+
+// THIRD SOLUTION
+function spEng3(s){
+    return /english/i.test(s);
+}
