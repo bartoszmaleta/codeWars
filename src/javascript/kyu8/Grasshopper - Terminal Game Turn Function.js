16350265eef24acd4e9f74fbd832e199e7b7e619
Bartosz Maleta <bartosz.maleta@gmail.com>
2020-07-16 14:00:36 +0200

    Add another solution + add simple fixing bug in if/else

--- src/javascript/kyu8/Grasshopper - Terminal Game Turn Function.js
+++ src/javascript/kyu8/Grasshopper - Terminal Game Turn Function.js
@@ -8,3 +8,9 @@ function doTurn () {
     printStatus();
 
 }
+
+// SECOND SOLUTION
+function doTurn () {
+    var actions = [rollDice, move, combat, getCoins, buyHealth, printStatus];
+    actions.forEach(e => e());
+}
