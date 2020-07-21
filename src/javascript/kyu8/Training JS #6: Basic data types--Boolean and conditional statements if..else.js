b5bdd1802300dfcf4e0ac7f596483a9d06d23151
Bartosz Maleta <bartosz.maleta@gmail.com>
2020-07-21 14:53:02 +0200

    Add other soultions

--- src/javascript/kyu8/Training JS #6: Basic data types--Boolean and conditional statements if..else.js
+++ src/javascript/kyu8/Training JS #6: Basic data types--Boolean and conditional statements if..else.js
@@ -1,3 +1,13 @@
 function trueOrFalse(val) {
     return val ? true.toString() : false.toString();
 }
+
+// SECOND SOLUTION
+function trueOrFalse2(val){
+    return Boolean(val).toString();
+}
+
+// THIRD SOLUTION
+function trueOrFalse3(val){
+    return !val ? 'false':'true'
+}
