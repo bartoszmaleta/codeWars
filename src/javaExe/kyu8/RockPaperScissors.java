1eea5f668014625423504230ded348c5d00e6267
Bartosz Maleta <bartosz.maleta@gmail.com>
2020-08-01 01:48:19 +0200

    Add simple rock paper scissor calculator win

---
+++ src/javaExe/kyu8/RockPaperScissors.java
@@ -0,0 +1,13 @@
+public class RockPaperScissors {
+
+  public static String rps(String p1, String p2) {
+    return p1.equals(p2) ? "Draw!" : "Player " + ("scissorspaper paperrock rockscissors".contains(p1+p2)?1:2) + " won!";
+  }
+
+  // SECOND SOLUTION
+ public static String rps(String p1, String p2) {
+    if(p1 == p2) return "Draw!";
+    int p = (p1 + p2).equals("scissorspaper") || (p1 + p2).equals("rockscissors") || (p1 + p2).equals("paperrock") ? 1 : 2;
+    return "Player " + p + " won!";
+  }
+}
