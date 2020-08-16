61d694816a3138b97582ff001db297a3e83b74e6
Bartosz Maleta <bartosz.maleta@gmail.com>
2020-08-16 12:53:23 +0200

    Add another options: betters for and stream

--- src/javaExe/kyu8/ArrayElementsWithCommaDelimeteresANYTYPE.java
+++ src/javaExe/kyu8/ArrayElementsWithCommaDelimeteresANYTYPE.java
@@ -1,5 +1,8 @@
 package javaExe.kyu8;
 
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
 public class ArrayElementsWithCommaDelimeteresANYTYPE {
     public static String printArray(Object array[]) {
         String result = "";
@@ -17,5 +20,22 @@ public class ArrayElementsWithCommaDelimeteresANYTYPE {
     public static void main(String[] args) {
         System.out.println(printArray(new Integer[]{2, 4, 5, 2}));
 
+    }
+
+    // SECOND OPTION
+    public static String printArray2(Object[] array) {
+        StringBuilder sb = new StringBuilder(7);
+        int i = 0;
+        for(; i < array.length - 1; i++){
+            sb.append(array[i]);
+            sb.append(",");
+        }
+        sb.append(array[i]);
+        return sb.toString();
+    }
+
+    // THIRD OPTION
+    public static String printArray3(Object[] array) {
+        return Arrays.stream(array).map(Object::toString).collect(Collectors.joining(","));
     }
 }
