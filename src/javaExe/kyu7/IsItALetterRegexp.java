229b31c621e8fa37ed8cfaa13692aabf61b7a5af
Bartosz Maleta <bartosz.maleta@gmail.com>
2020-08-25 14:23:04 +0200

    Add isLetter() implementation antoher option

--- src/javaExe/kyu7/IsItALetterRegexp.java
+++ src/javaExe/kyu7/IsItALetterRegexp.java
@@ -4,4 +4,9 @@ public class IsItALetterRegexp {
     public static boolean isLetter(String s) {
         return s.length() == 1 && Character.isLetter(s.charAt(0));
     }
+
+    // SECOND OPTION
+    public static boolean isLetter2(String s) {
+        return s.matches("[a-zA-Z]");
+    }
 }
