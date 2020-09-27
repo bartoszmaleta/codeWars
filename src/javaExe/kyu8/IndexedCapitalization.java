f3af172e07103f75e4ce83d2f97d893001f4c203
Bartosz Maleta <bartosz.maleta@gmail.com>
2020-09-27 18:32:29 +0200

    Capitalize chars at given index

--- src/javaExe/kyu8/IndexedCapitalization.java
+++ src/javaExe/kyu8/IndexedCapitalization.java
@@ -1,4 +1,23 @@
 package javaExe.kyu8;
 
+import java.util.Arrays;
+
 public class IndexedCapitalization {
+    public static String capitalize(String s, int[] ind){
+        char[] arrS = s.toCharArray();
+        for (int index : ind) {
+            if (index > s.length()) {
+                continue;
+            }
+            arrS[index] = Character.toUpperCase(arrS[index]);
+        }
+        return new String(arrS);
+    }
+
+    // SECOND OPTION
+    public static String capitalize2(String s, int[] ind) {
+        char[] chars = s.toCharArray();
+        Arrays.stream(ind).filter(c -> c < s.length()).forEach(c -> chars[c] = Character.toUpperCase(chars[c]));
+        return new String(chars);
+    }
 }
