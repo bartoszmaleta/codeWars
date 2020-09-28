1bad26a29bd7bce3cda70576913f7ef52d9a5ce0
Bartosz Maleta <bartosz.maleta@gmail.com>
2020-09-28 14:33:11 +0200

    Add get nth even number

--- src/javaExe/kyu8/GetNthEvenNumber.java
+++ src/javaExe/kyu8/GetNthEvenNumber.java
@@ -2,14 +2,6 @@ package javaExe.kyu8;
 
 public class GetNthEvenNumber {
     public static int nthEven(int n) {
-        int counter = 0;
-        int evenNum = Integer.MIN_VALUE;
-        for (int i = 0; counter < n; i++) {
-            if (i % 2 == 0) {
-                counter++;
-                evenNum = i;
-            }
-        }
-        return evenNum;
+        return (n * 2) - 2;
     }
 }
