5916285e07f946bbbeedd0efda54ad4c58f01627
Bartosz Maleta <bartosz.maleta@gmail.com>
2020-09-28 21:05:43 +0200

    Add override hashCode() + toString() + equals()

---
+++ src/javaExe/kyu8/NullObject.java
@@ -0,0 +1,19 @@
+package javaExe.kyu8;
+
+public class NullObject {
+    @Override
+    public int hashCode() {
+        return 0;
+    }
+
+    @Override
+    public String toString() {
+        return "null";
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        NullObject nullObject = (NullObject) obj;
+        return obj == null || (nullObject.hashCode() == this.hashCode());
+    }
+}
