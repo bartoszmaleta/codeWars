e2a626c166df4afd3345569f053c8b752d3486f4
Bartosz Maleta <bartosz.maleta@gmail.com>
2020-10-01 17:24:20 +0200

    Add change int to negative

--- src/javaExe/kyu8/ReturnNegative.java
+++ src/javaExe/kyu8/ReturnNegative.java
@@ -1,5 +1,7 @@
 package javaExe.kyu8;
 
+import static java.lang.Math.abs;
+
 public class ReturnNegative {
 
     public static int makeNegative(final int x) {
@@ -9,5 +11,9 @@ public class ReturnNegative {
     // SECOND OPTION
     public static int makeNegative2(final int x) {
         return x <= 0 ? x : Math.negateExact(x);
+    }
+
+    public static int makeNegative3(final int x) {
+        return -abs(x);
     }
 }
