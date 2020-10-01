2f7ceac420e352d7fe7cbeb48d9ca8c887deea1e
Bartosz Maleta <bartosz.maleta@gmail.com>
2020-10-01 17:44:57 +0200

    Add split s, sort char array, join to String

--- src/javaExe/kyu7/OrderingTheWords.java
+++ src/javaExe/kyu7/OrderingTheWords.java
@@ -5,6 +5,7 @@ import java.util.Arrays;
 import java.util.stream.Collectors;
 
 public class OrderingTheWords {
+
     public String orderWord(String s){
         if (s == null || s.isEmpty()) return "Invalid String!";
 
@@ -15,4 +16,18 @@ public class OrderingTheWords {
                 .mapToObj(intValue -> String.valueOf((char) intValue))
                 .collect(Collectors.joining(""));
     }
+
+    // SECOND OPTION
+    public String orderWord2(String s){
+        if(s==null||"".equals(s))return "Invalid String!";
+        char [] a = s.toCharArray();
+        java.util.Arrays.sort(a);
+        return String.valueOf(a);
+    }
+
+    // THIRD OPTION
+    public String orderWord3(String s) {
+        return (s == null || s.length() == 0) ? "Invalid String!" : s.chars().sorted().mapToObj(c -> String.valueOf((char) c)).collect(Collectors.joining());
+    }
+
 }
