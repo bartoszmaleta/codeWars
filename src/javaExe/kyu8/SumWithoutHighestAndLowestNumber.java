c049559cd32e21abf9f75e5ac9e611b7646ee496
Bartosz Maleta <bartosz.maleta@gmail.com>
2020-10-05 10:00:17 +0200

    Add second solution of sum of array without lowest and highest number

--- src/javaExe/kyu8/SumWithoutHighestAndLowestNumber.java
+++ src/javaExe/kyu8/SumWithoutHighestAndLowestNumber.java
@@ -2,11 +2,18 @@ package javaExe.kyu8;
 
 import java.util.Arrays;
 
+import static java.util.stream.IntStream.of;
+
 public class SumWithoutHighestAndLowestNumber {
     public static int sum(int[] numbers) {
         if (numbers == null || numbers.length <= 1) return 0;
         Arrays.sort(numbers);
 
         return Arrays.stream(numbers).sum() - numbers[0] - numbers[numbers.length - 1];
+    }
+
+    // SECOND OPTION
+    public static int sum2(int[] numbers) {
+        return (numbers == null || numbers.length <= 1) ? 0 : of(numbers).sum() - of(numbers).max().getAsInt() - of(numbers).min().getAsInt();
     }
 }
