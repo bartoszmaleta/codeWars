c40d442d8b82c443c1c9c55555e8322d79dfa740
Bartosz Maleta <bartosz.maleta@gmail.com>
2020-10-06 15:23:58 +0200

    Add convert km/h to cm/s + even number validator

---
+++ src/javaExe/kyu8/IsItEven.java
@@ -0,0 +1,8 @@
+package javaExe.kyu8;
+
+public class IsItEven {
+    public boolean isEven(double n) {
+        return n % 2 == 0;
+    }
+    
+}
--- src/javaExe/kyu8/KmPerHourToCentimetersPerSeconds.java
+++ src/javaExe/kyu8/KmPerHourToCentimetersPerSeconds.java
@@ -4,5 +4,5 @@ public class KmPerHourToCentimetersPerSeconds {
     public int cockroachSpeed(double x){
         return (int)(x / 0.036);
     }
-    
+
 }
