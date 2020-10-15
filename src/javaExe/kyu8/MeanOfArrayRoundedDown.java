63283df7f12399bff62018612109aef94ffe2670
Bartosz Maleta <bartosz.maleta@gmail.com>
2020-10-15 15:01:27 +0200

    Add mean of int array rounded down, stream method with isPresent() solution

--- src/javaExe/kyu8/MeanOfArrayRoundedDown.java
+++ src/javaExe/kyu8/MeanOfArrayRoundedDown.java
@@ -6,4 +6,9 @@ public class MeanOfArrayRoundedDown {
     public static int getAverage(int[] marks){
         return (int) Math.floor(Arrays.stream(marks).average().getAsDouble());
     }
+
+    // SECOND OPTION
+    public static int getAverage2(int[] marks){
+        return (int) Arrays.stream(marks).average().orElse(Double.NaN);
+    }
 }
