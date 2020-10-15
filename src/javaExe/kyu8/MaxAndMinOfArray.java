f1f40b3a62d53fb949c1dabd1e4d02f6f51f2b71
Bartosz Maleta <bartosz.maleta@gmail.com>
2020-10-15 14:16:16 +0200

    Add max of array with stream with isPresent() check

--- src/javaExe/kyu8/MaxAndMinOfArray.java
+++ src/javaExe/kyu8/MaxAndMinOfArray.java
@@ -1,5 +1,6 @@
 package javaExe.kyu8;
 
+import java.security.InvalidAlgorithmParameterException;
 import java.util.Arrays;
 
 public class MaxAndMinOfArray {
@@ -7,7 +8,18 @@ public class MaxAndMinOfArray {
         return Arrays.stream(list).min().orElse(0);
     }
 
-    public int max(int[] list) {
+    public int max(int[] list) throws InvalidAlgorithmParameterException {
         return Arrays.stream(list).max().orElse(0);
+    }
+
+
+
+    // SECOND OPTION
+    public int min2(int[] list) throws InvalidAlgorithmParameterException {
+        return Arrays.stream(list).min().orElseThrow(InvalidAlgorithmParameterException::new);
+    }
+
+    public int max2(int[] list) throws InvalidAlgorithmParameterException {
+        return Arrays.stream(list).max().orElseThrow(InvalidAlgorithmParameterException::new);
     }
 }
