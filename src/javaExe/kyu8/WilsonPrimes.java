9b77593f103944d447b38fb797b235333db1acef
Bartosz Maleta <bartosz.maleta@gmail.com>
2020-10-17 20:17:23 +0200

    Add is wilson prime

--- src/javaExe/kyu8/WilsonPrimes.java
+++ src/javaExe/kyu8/WilsonPrimes.java
@@ -2,28 +2,16 @@ package javaExe.kyu8;
 
 public class WilsonPrimes {
 
-    // TODO:
-    public static boolean am_i_wilson2(double n) {
-        double number = (factorial((long) (n - 1)) + 1) / (n * n);
-        System.out.println(number);
-        return true;
+    public static boolean am_i_wilson(long n) {
+        return n == 5 || n == 13 || n == 563;
     }
 
-    public static boolean am_i_wilson(double n) {
-        return (factorial((long) (n - 1)) + 1) / (n * n) % 1 == 0;
-    }
-
-    public static long factorial(long number) {
-        return number <= 1 ? 1 : number * factorial(number - 1);
-    }
-
-    public static void main(String[] args) {
-        am_i_wilson2(0);
-        am_i_wilson2(1);
-        am_i_wilson2(5);
-
-        am_i_wilson(0);
-        am_i_wilson(1);
-        am_i_wilson(5);
+    // SECOND OPTION
+    public static boolean am_i_wilson2(long n) {
+        long modulus = n * n;
+        long product = 1;
+        for (long factor = 2; factor < n; factor++)
+            product = (product * factor) % modulus;
+        return product + 1 == modulus;
     }
 }
