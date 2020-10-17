ca880e9f7acbac206be93fcf9aa3852a210e00f4
Bartosz Maleta <bartosz.maleta@gmail.com>
2020-10-17 19:38:20 +0200

    Add simple bug fix

---
+++ src/javaExe/kyu8/PushAHashIntoArray.java
@@ -0,0 +1,12 @@
+package javaExe.kyu8;
+
+import java.util.List;
+import java.util.ArrayList;
+
+public class PushAHashIntoArray {
+    public static List<String> push(){
+        List<String> items = new ArrayList<>();
+        items.add("an object");
+        return items;
+    }
+}
