e6bcf31fdbfe697d9b21ed3f725a7344920038ee
Bartosz Maleta <bartosz.maleta@gmail.com>
2020-10-17 20:03:16 +0200

    Add simple bug fix

---
+++ src/javaExe/kyu8/GrasshopperDebug.java
@@ -0,0 +1,8 @@
+package javaExe.kyu8;
+
+public class GrasshopperDebug {
+    static String weatherInfo(int temp) {
+        double c = (temp - 32) * 5.0 / 9;
+        return c + " is " + (c > 0 ? "above " : "") + "freezing temperature";
+    }
+}
