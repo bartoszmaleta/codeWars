538941b3e05b16358e975fb2d5950ece367b542b
Bartosz Maleta <bartosz.maleta@gmail.com>
2020-11-12 23:47:33 +0100

    Add simple addition variable

---
+++ src/javaExe/kyu8/MessiGoals.java
@@ -0,0 +1,11 @@
+package javaExe.kyu8;
+
+public class MessiGoals {
+    class Goals {
+        public int laLigaGoals = 43;
+        public int championsLeagueGoals = 10;
+        public int copaDelReyGoals = 5;
+
+        public int totalGoals = laLigaGoals + championsLeagueGoals + copaDelReyGoals;
+    }
+}
