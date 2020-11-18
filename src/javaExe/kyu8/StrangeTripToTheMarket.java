7e752a3d2c103ad918812948ddea54aa423bea9b
Bartosz Maleta <bartosz.maleta@gmail.com>
2020-11-18 16:12:43 +0100

    Add contains() in String

---
+++ src/javaExe/kyu8/StrangeTripToTheMarket.java
@@ -0,0 +1,7 @@
+package javaExe.kyu8;
+
+public class StrangeTripToTheMarket {
+    public static boolean isLockNessMonster(String s){
+        return s.contains("tree fiddy") || s.contains("3.50");
+    }
+}
