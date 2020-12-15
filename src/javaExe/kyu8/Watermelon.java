56155285a7a46caf4ab3c1e1ecd5e5c0c634d072
Bartosz Maleta <bartosz.maleta@gmail.com>
2020-12-15 13:37:02 +0100

    Add simple even number validator

--- src/javaExe/kyu8/Watermelon.java
+++ src/javaExe/kyu8/Watermelon.java
@@ -1,2 +1,17 @@
-package javaExe.kyu8;public class Watermelon {
+package javaExe.kyu8;
+
+public class Watermelon {
+    public static boolean divide(int weight) {
+        return weight >= 3 && (weight % 2 == 0);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(divide(4));
+        System.out.println(divide(2));
+        System.out.println(divide(5));
+        System.out.println(divide(88));
+        System.out.println(divide(100));
+        System.out.println(divide(67));
+        System.out.println(divide(90));
+    }
 }
