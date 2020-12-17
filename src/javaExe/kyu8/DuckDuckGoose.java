8277bf11d66aac364e81cece81387742d51cf4de
Bartosz Maleta <bartosz.maleta@gmail.com>
2020-12-17 14:27:31 +0100

    Add iterating over and over array

---
+++ src/javaExe/kyu8/DuckDuckGoose.java
@@ -0,0 +1,22 @@
+package javaExe.kyu8;
+
+public class DuckDuckGoose {
+
+    // IT IS WORKING, BUT LACKS CLASS 'PLAYER'.
+
+
+//    public static String duckDuckGoose3(Player[] players, int goose) {
+//        if (goose == 1) return players[0].name;
+//        int counter = 1;
+//        Player player = players[0];
+//        for (int i = 0; i < players.length; i++) {
+//            player = players[i];
+//            if (counter == goose) break;
+//            if (i + 1 == players.length) {
+//                i = -1;
+//            }
+//            counter++;
+//        }
+//        return player.name;
+//    }
+}
