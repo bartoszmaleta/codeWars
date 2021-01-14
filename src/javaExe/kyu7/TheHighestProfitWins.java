e20871d87f09d4ba46f7d5403231c6af28d65231
Bartosz Maleta <bartosz.maleta@gmail.com>
2021-01-14 12:31:05 +0100

    Add min and max integer of array

--- src/javaExe/kyu7/TheHighestProfitWins.java
+++ src/javaExe/kyu7/TheHighestProfitWins.java
@@ -6,4 +6,10 @@ public class TheHighestProfitWins {
     public static int[] minMax(int[] arr) {
         return new int[] {Arrays.stream(arr).min().getAsInt(), Arrays.stream(arr).max().getAsInt()};
     }
+
+    // SECOND OPTION
+    public static int[] minMax2(int[] arr) {
+        Arrays.sort(arr);
+        return new int[]{arr[0],arr[arr.length-1]};
+    }
 }
