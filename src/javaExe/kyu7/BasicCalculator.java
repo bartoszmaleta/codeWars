b37ef493a11eec1df61f2e6370d5eac3c9cc7d01
Bartosz Maleta <bartosz.maleta@gmail.com>
2021-02-02 14:39:35 +0100

    Add simple calculator with some robustness

---
+++ src/javaExe/kyu7/BasicCalculator.java
@@ -0,0 +1,17 @@
+package javaExe.kyu7;
+
+public class BasicCalculator {
+    public static Double calculate(final double numberOne, final String operation, final double numberTwo) {
+        switch (operation){
+            case "+":
+                return numberOne + numberTwo;
+            case "-":
+                return numberOne - numberTwo;
+            case "*":
+                return (numberOne * numberTwo + 0.0);
+            case "/":
+                return numberTwo == 0 ? null : numberOne / numberTwo;
+            default: return null;
+        }
+    }
+}
